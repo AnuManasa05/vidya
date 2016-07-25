d40d4d87d25a0902124a7546fea502139d499877
AnuManasa05 <anucse2k11@gmail.com>
2016-07-25 18:33:03 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-apigeetool deploynodeapp -u 'spoola@miraclesoft.com' -p 'Miracle@001' -o sreevidya -e test -n 'vidya' -d . -m hello-world-server.js -b /vidya -R --verbose --debug
-echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/vidya in cURL or your browser"
+apigeetool deploynodeapp -u 'anucse2k11@gmail.com' -p 'AnuManasa18' -o anumanasa -e test -n 'example5' -d . -m hello-world-server.js -b /example5 -R --verbose --debug
+echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/example5 in cURL or your browser"
 
