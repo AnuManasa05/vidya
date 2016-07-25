12b720e8b4f792d1013fe53b9e6f4c27ebd4a923
AnuManasa05 <anucse2k11@gmail.com>
2016-07-25 04:31:52 -0700

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,3 @@
+apigeetool deploynodeapp -u 'spoola@miraclesoft.com' -p 'Miracle@001' -o sreevisya -e test -n 'vidya' -d . -m hello-world-server.js -b /vidya -R --verbose --debug
+echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/vidya in cURL or your browser"
+
